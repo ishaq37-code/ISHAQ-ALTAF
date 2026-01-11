1b17a5df27b81709f5e3cdbf83d023da0d1756e3
ishaq37-code <ishaqaltaf67@gmail.com>
2026-01-12 02:23:45 +0500

    changes

--- indesx.js
+++ indesx.js
@@ -1,16 +1,56 @@
-const reumeBtn= document.querySelectorAll(".resume-btn")
+const reumeBtn = document.querySelectorAll(".resume-btn")
 
-reumeBtn.forEach((btn,idx)=>{
-    btn.addEventListener("click",()=>{
-           const resumeDetails=document.querySelectorAll(".resume-details");
+reumeBtn.forEach((btn, idx) => {
+    btn.addEventListener("click", () => {
+        const resumeDetails = document.querySelectorAll(".resume-details");
 
-        reumeBtn.forEach(btn =>{
+        reumeBtn.forEach(btn => {
             btn.classList.remove("active");
         });
         btn.classList.add("active")
-        resumeDetails.forEach(deatils =>{
+        resumeDetails.forEach(deatils => {
             deatils.classList.remove("active");
         });
-         resumeDetails[idx].classList.add('active');
+        resumeDetails[idx].classList.add('active');
+    });
 });
-});
+
+const arrowLeft = document.querySelector(".protfolio-box .navgation .left");
+const arrowRight = document.querySelector(".protfolio-box .navgation .right");
+
+let index = 0;
+
+const activeProtfolio = () => {
+    const imageSlide = document.querySelector('.image-carasoul .image-slide');
+    const protFolio=document.querySelectorAll('.protfolio-details')
+    imageSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+ 
+
+ protFolio.forEach(details=>{
+   details.classList.remove("active")
+ });
+ protFolio[index].classList.add("active")
+};
+arrowRight.addEventListener('click', () => {
+    if (index < 2) {
+        index++;
+        arrowLeft.classList.remove('disabled');
+    }
+    else {
+        index = 2;
+        arrowRight.classList.add('disabled')
+    }
+    activeProtfolio()
+})
+arrowLeft.addEventListener('click', () => {
+    if (index > 0) {
+        index--;
+        arrowRight.classList.remove('disabled')
+    }
+    else {
+        index = 0;
+        arrowLeft.classList.add('disabled');
+        
+    }
+    activeProtfolio()
+})
